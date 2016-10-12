aa1560427064deec19746305e8446287bf7a8e19
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-12 20:20:44 -0200

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#*-*coding: utf-8 *-*
+#Autor: EdwardRamos
+#Date: 10/12/2016 U.S.A
+
+
+#Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e
+#continue pedindo até que o usuário informe um valor válido.
+
+
+nota = float(input("Nota: "))
+while nota >= 10:
+    print("(+) Invalido")
+    nota = float(input("Nota: "))
+print("(+) Valor valido: %r" % nota)
