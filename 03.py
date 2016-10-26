f4afa41e587429880e1dda3c788ac9405c670045
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-26 00:29:01 -0200

    Update 03.py

--- 03.py
+++ 03.py
@@ -4,15 +4,14 @@
 #Date: 10/12/2016 U.S.A
 
 
-
-#Faça um programa que leia e valide as seguintes informações:
-
-#    Nome: maior que 3 caracteres;
-#    Idade: entre 0 e 150;
-#    Salário: maior que zero;
-#    Sexo: 'f' ou 'm';
-#    Estado Civil: 's', 'c', 'v', 'd'; 
-
+"""
+Faça um programa que leia e valide as seguintes informações:
+Nome: maior que 3 caracteres;
+Idade: entre 0 e 150;
+Salário: maior que zero;
+Sexo: 'f' ou 'm';
+Estado Civil: 's', 'c', 'v', 'd'; 
+"""
 
 nome = ''
 idade = 0
