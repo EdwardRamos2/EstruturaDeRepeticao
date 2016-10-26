7d187ac5e0e7b48000ed888880d4a628aa4307b1
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-26 00:23:43 -0200

    Update 01.py

--- 01.py
+++ 01.py
@@ -3,10 +3,10 @@
 #Autor: EdwardRamos
 #Date: 10/12/2016 U.S.A
 
-
-#Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e
-#continue pedindo até que o usuário informe um valor válido.
-
+"""
+Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e
+continue pedindo até que o usuário informe um valor válido.
+"""
 
 nota = float(input("Nota: "))
 while nota >= 10:
