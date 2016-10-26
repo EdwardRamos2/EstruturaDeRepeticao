78ec4f5bae1aacf66eb9cfa362ca837b30c60e99
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-26 08:05:47 -0300

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,12 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+#Autor: Edward Ramos
+#Date: 10/26/2016 08:05AM
+print("This is console module")
+
+num = int(input('insert num: '))
+print('Tabuada de: %d' % num)
+for i in range(11):
+     print(num,'x',i,'=',num*i)
+     
