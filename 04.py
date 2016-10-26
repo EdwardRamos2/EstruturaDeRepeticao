dbe7e910c5e3c0ab3ec2b66c99422ea57589f182
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-26 00:23:21 -0200

    Update 04.py

--- 04.py
+++ 04.py
@@ -3,12 +3,12 @@
 #Autor: EdwardRamos
 #Date: 10/25/2016 U.S.A
 
-
-#Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% 
-#e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. 
-#Faça um programa que calcule e escreva o número de anos necessários para que a população do país 
-#A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento. 
-
+"""
+Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% 
+e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. 
+Faça um programa que calcule e escreva o número de anos necessários para que a população do país 
+A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento. 
+"""
 
 populacaoA,populacaoB,anos  = 80000, 200000, 0
 crescimentoA,crescimentoB = 0.03, 0.015
